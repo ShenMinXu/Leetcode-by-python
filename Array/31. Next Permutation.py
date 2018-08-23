f339de01091ff4e0194660a9ebf3a226f9419241
ShenMinXu <452280994@qq.com>
2018-08-23 11:32:01 +0800

    a

--- Array/31. Next Permutation.py
+++ Array/31. Next Permutation.py
@@ -17,4 +17,38 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 1,2,3 → 1,3,2
 3,2,1 → 1,2,3
 1,1,5 → 1,5,1
+
+
+这道题是给定一个数组和一个排列，求下一个排列。算法上其实没有什么特别的地方，主要的问题是经常不是一见到这个题就能马上理清思路。下面我们用一个例子来说明，比如排列是(2,3,6,5,4,1)，求下一个排列的基本步骤是这样：
+1) 先从后往前找到第一个不是依次增长的数，记录下位置p。比如例子中的3，对应的位置是1;
+2) 接下来分两种情况：
+    (1) 如果上面的数字都是依次增长的，那么说明这是最后一个排列，下一个就是第一个，其实把所有数字反转过来即可(比如(6,5,4,3,2,1)下一个是(1,2,3,4,5,6));
+    (2) 否则，如果p存在，从p开始往后找，找到下一个数就比p对应的数小的数字，然后两个调换位置，比如例子中的4。调换位置后得到(2,4,6,5,3,1)。最后把p之后的所有数字倒序，比如例子中得到(2,4,1,3,5,6), 这个即是要求的下一个排列。
+以上方法中，最坏情况需要扫描数组三次，所以时间复杂度是O(3*n)=O(n)，空间复杂度是O(1)。代码如下：
+
 '''
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        # Use two-pointers: two pointers start from back
+        # first pointer j stop at descending point
+        # second pointer i stop at value > nums[j]
+        # swap and sort rest
+        if not nums: return None
+        i = len(nums)-1
+        j = -1 # j is set to -1 for case `4321`, so need to reverse all in following step
+        while i > 0:
+            if nums[i-1] < nums[i]: # first one violates the trend
+              j = i-1
+              break
+            i-=1
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] > nums[j]: #
+                nums[i], nums[j] = nums[j], nums[i] # swap position
+                nums[j+1:] = sorted(nums[j+1:]) # sort rest
+                return
+
+
